aa5aac1fbefb1a96a3b48e88bedfa288c7e6e6c5
Tommy Myers <tommy@tommymyers.dev>
2021-06-06 13:31:06 +0100

    uibuild

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
-app.use(express.static("build"));
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
